6eb8acbd90eda0a4ec4722ca45ca9e3ada5ad8ee
Miguel Teixeira <77354507+micatalyst@users.noreply.github.com>
2023-12-20 04:45:57 +0000

    Add files via upload

---
+++ src/app/sorteio/participantes.js
@@ -0,0 +1,72 @@
+const participantes = [
+  { nome: "Soraia", apelido: "Torres", num: "001" },
+  { nome: "Caroline", apelido: "Freitas", num: "002" },
+  { nome: "Petra", apelido: "Naydenov", num: "003" },
+  { nome: "Bianca", apelido: "Querino", num: "004" },
+  { nome: "Joana", apelido: "Martins", num: "005" },
+  { nome: "Juliana", apelido: "Alves", num: "017" },
+  { nome: "João", apelido: "Tavares", num: "020" },
+  { nome: "Inês", apelido: "Simão", num: "024" },
+  { nome: "Maria", apelido: "Araujo", num: "025" },
+  { nome: "Tiago", apelido: "Neves", num: "029" },
+  { nome: "Joana", apelido: "Estebainha", num: "030" },
+  { nome: "Mariana", apelido: "Nunes", num: "031" },
+  { nome: "Júlia", apelido: "João", num: "032" },
+  { nome: "Alexandre", apelido: "Fundinho", num: "035" },
+];
+
+export default participantes;
+
+// Dia 6
+
+/* { nome: "Caroline", apelido: "Freitas", num: "002" },
+  { nome: "Petra", apelido: "Naydenov", num: "003" },
+  { nome: "Bianca", apelido: "Querino", num: "004" },
+  { nome: "Ana", apelido: "Ferreira", num: "007" },
+  { nome: "Mariana", apelido: "Aguiar", num: "008" },
+  { nome: "Vinícius", apelido: "Paula", num: "009" },
+  { nome: "Simão", apelido: "Pandeirada", num: "010" },
+  { nome: "Paula", apelido: "Lacerda", num: "011" },
+  { nome: "Feriel", apelido: "Abid", num: "012" },
+  { nome: "Beatriz", apelido: "Agostinho", num: "014" },
+  { nome: "Diana", apelido: "Abreu", num: "018" },
+  { nome: "Manuel", apelido: "Paulino", num: "019" },
+  { nome: "Laura", apelido: "Paciência", num: "021" },
+  { nome: "Simão", apelido: "Silva", num: "022" },
+  { nome: "Juliana", apelido: "Santos", num: "023" },
+  { nome: "Inês", apelido: "Simão", num: "024" },
+  { nome: "Maria", apelido: "Araujo", num: "025" },
+  { nome: "Eva", apelido: "Martins", num: "026" }, */
+
+// Dia 14
+
+/* { nome: "Caroline", apelido: "Freitas", num: "002" },
+  { nome: "Petra", apelido: "Naydenov", num: "003" },
+  { nome: "Bianca", apelido: "Querino", num: "004" },
+  { nome: "Joana", apelido: "Martins", num: "005" },
+  { nome: "Júnio", apelido: "Jardim", num: "006" },
+  { nome: "Leonardo", apelido: "Gaibor", num: "015" },
+  { nome: "Martim", apelido: "Tabanez", num: "016" },
+  { nome: "Inês", apelido: "Simão", num: "024" },
+  { nome: "Maria", apelido: "Araujo", num: "025" },
+  { nome: "Patrícia", apelido: "Cruz", num: "028" },
+  { nome: "Sofia", apelido: "Silva", num: "027" },
+  { nome: "Carolina", apelido: "Machado", num: "033" },
+  { nome: "Carolina", apelido: "Machado", num: "033" }, */
+
+// Dia 20
+
+/* { nome: "Soraia", apelido: "Torres", num: "001" },
+  { nome: "Caroline", apelido: "Freitas", num: "002" },
+  { nome: "Petra", apelido: "Naydenov", num: "003" },
+  { nome: "Bianca", apelido: "Querino", num: "004" },
+  { nome: "Joana", apelido: "Martins", num: "005" },
+  { nome: "Juliana", apelido: "Alves", num: "017" },
+  { nome: "João", apelido: "Tavares", num: "020" },
+  { nome: "Inês", apelido: "Simão", num: "024" },
+  { nome: "Maria", apelido: "Araujo", num: "025" },
+  { nome: "Tiago", apelido: "Neves", num: "029" },
+  { nome: "Joana", apelido: "Estebainha", num: "030" },
+  { nome: "Mariana", apelido: "Nunes", num: "031" },
+  { nome: "Júlia", apelido: "João", num: "032" },
+  { nome: "Alexandre", apelido: "Fundinho", num: "035" }, */
